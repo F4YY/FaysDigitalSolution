beb8c81f90adcc97d76c1bd0c78e88d13621a353
Riki Wendri <fayyadh.farizzi@gmail.com>
2024-06-09 20:59:52 +0700

    add HeroTxtBtn component

--- builder-registry.js
+++ builder-registry.js
@@ -1,9 +1,14 @@
 "use client";
 import { builder, Builder } from "@builder.io/react";
 import Counter from "./components/Counter/Counter";
+import HeroTxtBtn from "./components/HeroTxtBtn";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
 
 Builder.registerComponent(Counter, {
   name: "Counter",
+});
+
+Builder.registerComponent(HeroTxtBtn, {
+  name: "HeroTxtBtn",
 });
---
+++ components/HeroTxtBtn.jsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import styled from "styled-components";
+
+function HeroTxtBtn() {
+  return (
+    <main>
+      <section>
+        <HeroContainer>
+          <HeroText>
+            <p>Custom Website solutions to help your business <span>succeed.</span></p>
+          </HeroText>
+          <StartButton>Get Started!</StartButton>
+        </HeroContainer>
+      </section>
+    </main>
+  );
+}
+
+const HeroContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 500px;
+  border-radius: 0 20% 20% 0;
+`;
+
+const HeroText = styled.div`
+  position: relative;
+  margin-top: 200px;
+  background-color: rgba(74, 74, 74, 0.65);
+  padding: 40px;
+  font: 45px "Poppins", sans-serif;
+  p {
+    color: #fff;
+    span {
+      color: #f7971a;
+    }
+  }
+`;
+
+const StartButton = styled.button`
+  all: unset;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  background-color: rgba(74, 144, 226, 1);
+  color: #fff;
+  border-radius: 4px;
+  text-align: center;
+  cursor: pointer;
+  font-size: 25px;
+  padding: 15px 25px;
+`;
+
+export default HeroTxtBtn;
